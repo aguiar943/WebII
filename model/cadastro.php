1debcce8a7497c7cd708bed9369265a862e1bd88
Rodrigo Stochero <rsaguiar@ucs.br>
2022-04-15 13:14:44 -0300

    funcoes
    adicionado tela de login, imagem, cadastro

--- model/cadastro.php
+++ model/cadastro.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<link rel="icon" href="imagens/simbolo.png" />
 		<title>
-			Login
+			Cadastro novos usuários
 		</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
@@ -16,41 +16,53 @@
       <a href="home.php" class="d-flex align-items-center text-dark text-decoration-none">
         <img src="imagens/logosemfundo.png" width="300" height="70"></img>
       </a>
+
+     <!-- <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-dark text-decoration-none" href="index.html">Home</a>
+		<a class="me-3 py-2 text-dark text-decoration-none" href="plano.html">Planos</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="login.html">Login</a>
+      </nav> -->
     </div>
   </header>
 	<div class="modal-dialog" role="document">
 		<div class="modal-content rounded-5 shadow">
 			<form class="p-4 p-md-5 border rounded-3 bg-light">
 				<div class="modal-header p-5 pb-4 border-bottom-0">
-					<h2 class="fw-bold mb-0">LOGIN</h2>
+					<h2 class="fw-bold mb-0">CADASTRO</h2>
 				</div>
 				<div class="form-floating mb-3">
-					<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+					<input type="name" class="form-control" id="floatingInput" placeholder="robero">
+					<label for="floatingInput">Nome</label>
+					
+				</div>
+				<div class="form-floating mb-3">
+					<input type="name" class="form-control" id="floatingInput" placeholder="robero">
+					<label for="floatingInput">Sobrenome</label>
+					
+				</div>
+				<div class="form-floating mb-3">
+					<input type="date" class="form-control" id="floatingInput" placeholder="date">
+					<label for="floatingInput">Nascimento</label>
+				</div>
+				<div class="form-floating mb-3">
+					<input type="email" class="form-control" id="floatingInput" placeholder="email">
 					<label for="floatingInput">Email</label>
 				</div>
 				<div class="form-floating mb-3">
 					<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
 					<label for="floatingPassword">Senha</label>
 				</div>
-				<div class="checkbox mb-3">
-				<label>
-					<input type="checkbox" value="remember-me"> Lembre-me
-					<br><a href='recupera_senha.php'>Esqueci minha senha</a>
-				</label>
-				</div>
-				<button class="w-100 btn btn-lg btn-primary" type="submit">Logar</button>
+				<button class="w-100 btn btn btn-success" type="submit">Cadastrar</button><br><br>
+				<button class="w-100 btn btn btn-danger" type="Reset">Cancelar</button><br>
 				<hr class="my-4">
 				<small class="text-muted"></small>
-				<a class="w-100 btn btn-secondary" href="cadastro.php">Cadastrar-se</a>
 			</form>
 		</div>
 	</div>
-
 <hr class="featurette-divider">
   <footer class="container">
-  <p>&copy; 2022–2022 Web II &middot; <a href="home.php">Compre Aí</a> &middot;</p>
+    <p>&copy; 2022–2022 Web II &middot; <a href="home.php">Compre Aí</a> &middot;</p>
   </footer>
-
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
