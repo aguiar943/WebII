7c290efcb8c805fa0540ccc413b12c8d00d5080a
mauricio-esuk <83623933+mauricio-esuk@users.noreply.github.com>
2022-04-17 20:53:11 -0300

    Add files via upload
    Criação do menu com três níveis de navegação contendo início, produtos, sobre, etc. Remoção da borda ao redor do logo, da barra de busca e das bordas ao redor dos ícones de carrinho e usuário.
    
    Retirado o código do menu, da barra de busca e dos ícones que estavam no index e transferidos para ficarem no header já que estes componentes e menus irão aparecer em todas as páginas.
    
    Retirado código do rodapé que estava no index e transferido para o arquivo footer php já que o rodapé aparecerá em todas as páginas

--- site/footer.php
+++ site/footer.php
@@ -9,10 +9,30 @@
     
     <body>
 -->
-        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="form-validation.js"></script>
 
-        <br>
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+
+        <p class="mb-1">&copy; 2017–2022 Compre Ai</p>
+
+        <ul class="list-inline">
+
+            <li class="list-inline-item"><a href="#">Privacy</a></li>
+
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+
+            <li class="list-inline-item"><a href="#">Support</a></li>
+
+        </ul>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="form-validation.js"></script>
+
+    <br>
 
     </body>
 
--- site/index.php
+++ site/index.php
@@ -10,176 +10,9 @@ include_once "fachada.php";
 
 -->
 
-	<?php
-		/*	
-		$dao = $factory->getVeiculoDao();
-		$veiculos = $dao->buscaTodos();
-		*/
-	?>
-
-	<div class=" text-center row " style="background-color: #d8d8d8;">
-
-		<!-- linha da barra superior fica com a coluna principal centralizada -->
-		
-		<div class="row mt-1 d-flex justify-content-center">
-
-			<!--coluna da linha fica com itens centralizados -->
-
-			<div class="col-xl-8 d-flex justify-content-center banner-h">
-
-				<div class = "col-xl-2 border border-dark d-flex justify-content-center mt-1 mb-1">
-
-					<div class = "col-xl-12" > 
-
-						<img src = "../imagens/logo.png" class = " w-100 h-100 mb-4" >
-						
-					</div>
-
-				</div>
-
-				<div class = "col-xl-7 border border-dark mt-1 mb-1">
-
-					<div class = "d-flex justify-content-start h-100 search-mg " >
-
-						<input type="text" class="w-100 ms-1 search-mg border border-danger" placeholder = "Buscar" />
-						
-
-						<button type="button" class="btn btn-danger lupa-mg">
-
-							<img src="../imagens/lupa.png" class="h-50 align-top mt-1" > <!-- search align-top -->
-
-						</button>
-							
-					</div>
-
-				</div>
-
-				<div class = "col-xl-1 border border-dark d-flex justify-content-center mt-1 mb-1">
-
-					<div class = "d-flex justify-content-start h-100 search-mg" >
-
-						<a href="#" class="d-flex align-items-center conta mt-2 ">
-
-							<img src="../imagens/carrinho.png" class="align-middle  h-100 mb-3 mt-2" >
-						
-						</a>
-						
-						
-						
-					</div>
-
-				</div>
-
-				<div class = "col-xl-2 border border-dark mt-1 mb-1">
-
-					<div class = "d-flex justify-content-start h-100 search-mg" >
-
-						<img src="../imagens/user.png" class="align-middle ms-2 mb-2 mt-1" >
-
-						<a href="#" class="d-flex align-items-center conta">
-
-							<span class="d-flex align-items-center ms-2 mt-1 mb-2 text-dark" style="font-size:1.4vh;">Minha Conta</span>
-
-						</a>
-
-					</div>
-
-
-
-				</div>
-
-			</div>
-			
-		</div>
-
-	</div>
-
-
 	<div class="container ">
-
+		
 		<main>
-			
-			<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center " >
-
-				<div class="collapse navbar-collapse" >
-
-					<div class="row mt-3  ">
-
-						<div class="col-xl-12 menu-h nav-item active d-flex justify-content-center">
-
-							<div class = "col-xl-2 border border-dark d-flex justify-content-center nav-item active">
-
-								<a class="nav-link d-flex align-items-center" href="#"> Início </a>
-
-							</div>
-
-							<div class = "col-xl-2 border border-dark d-flex justify-content-center round nav-item">
-
-								<a class="nav-link d-flex align-items-center" href="#"> Empresa </a>
-
-							</div>
-
-							<div class = "col-xl-2 border border-dark d-flex justify-content-center nav-item dropdown">
-
-								<a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" 
-									aria-haspopup="true" aria-expanded="false"
-								>
-
-									Produtos
-							
-								</a>
-								
-								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" 
-										aria-haspopup="true" aria-expanded="false"
-									>
-
-										Informática
-								
-									</a>
-
-									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-										<a class="dropdown-item" href="#">Action</a>
-
-										<a class="dropdown-item" href="#">Another action</a>
-
-									</div>
-
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" 
-										aria-haspopup="true" aria-expanded="false"
-									>
-
-										Vestuário
-								
-									</a>
-
-									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-										<a class="dropdown-item" href="#">Action</a>
-
-										<a class="dropdown-item" href="#">Another action</a>
-
-									</div>
-
-								</div>
-														
-							</div>
-
-							<div class = "col-xl-2 border border-dark d-flex justify-content-center nav-item">
-
-								<a class="nav-link d-flex align-items-center" href="#"> Cadastro </a>
-
-							</div>
-
-						</div>
-						
-					</div>	
-					
-				</div>
-			
-			</nav>		
 
 			<div class="row g-5 mt-4">
 				
@@ -194,13 +27,15 @@ include_once "fachada.php";
 						
 						<div class="row g-3">
 
-							Em construção...
+							Em construção ... [ 14 / 04 ~ 17 / 04/ ]
 
 							<hr>
 
 						</div>
 
-						<a href="editaVeiculo.php" class = "w-25 btn btn-primary btn-lg" >Novo</a>
+						<a href="#" class = " btn btn-primary btn-lg" >Início</a>
+
+						<!-- <a href="editaVeiculo.php" class = " btn btn-primary btn-lg" >Início</a> -->
 
 					</form>
 					
@@ -209,21 +44,6 @@ include_once "fachada.php";
 			</div>
 
 		</main>
-
-		<footer class="my-5 pt-5 text-muted text-center text-small">
-
-			<p class="mb-1">&copy; 2017–2021 Company Name</p>
-			<ul class="list-inline">
-
-			<li class="list-inline-item"><a href="#">Privacy</a></li>
-
-			<li class="list-inline-item"><a href="#">Terms</a></li>
-
-			<li class="list-inline-item"><a href="#">Support</a></li>
-
-			</ul>
-
-		</footer>
 
 	</div>
 
