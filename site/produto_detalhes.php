d7f8dcb38bb343e320885c709231bdc0a415ffc6
mauricio-esuk <83623933+mauricio-esuk@users.noreply.github.com>
2022-04-24 23:49:36 -0300

    Add files via upload
    Criação da página de detalhes do produto. Criação das áreas onde irão aparecer as fotos do produto e os detalhes informativos do produto ( nome, preço, cores, etc ) . Esta página ainda está sendo desenvolvida e posteriormente, serão adicionadas novas áreas, sessões e informações.

---
+++ site/produto_detalhes.php
@@ -0,0 +1,320 @@
+<?php include 'header.php.'; ?>
+
+<div id = "detalhes_produto" class = "row d-flex justify-content-center border border-dark " >
+
+
+    <div id = "foto_main" class = "col-xl-3 border border-dark h-100 ms-5" >
+
+        sdfdsf
+
+    </div>
+
+    <div id = "detalhes" class = "col-xl-5 w-50" >
+
+        <div id = "titulo" class = "row" >
+
+            <div id = "produto-info-h" class = "col-xl-5 mt-2 ms-2 w-50" >
+
+                Placa de Vídeo NVIDIA GeForce GTX 1050VV
+
+            </div>
+
+        </div>
+
+        <div id = "preco" class = "row mt-1" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+
+                <b> R$ 900,00 </b>
+
+            </div>
+
+        </div>
+
+        <div id = "parcelamento" class = "row mt-1" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+                
+                R$ 6 x R$ 150 sem juros
+
+            </div>
+
+        </div>
+
+        <div id = "lb_cores" class = "row" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+
+                Cores: XFX-RX580-8G-2304SP
+
+            </div>
+
+        </div>
+
+        <div id = "detalhes_produto" class = "row  mini-fotos-area d-flex align-items-center" >
+
+            <div id = "cor_1" class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex justify-content-center" >
+
+                Cor1
+
+            </div>
+
+            <div id = "cor_2" class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex justify-content-center" >
+
+                Cor2
+
+            </div>
+
+            <div id = "cor_3" class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex justify-content-center" >
+            
+                Cor3
+
+            </div>
+
+        </div>
+
+        <div id = "lb_qtd" class = "row " >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+
+                Quantidade:
+
+            </div>
+
+        </div>
+
+        <div id = "btns_qtd" class = "row mini-fotos-area d-flex align-items-center" >
+
+            <button 
+                id = "diminuir" 
+                class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex align-items-center justify-content-center" 
+            >
+
+                <i class="fa-solid fa-2x fa-minus"></i>
+
+            </button>
+
+            <input 
+                id = "qtd" 
+                class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex align-items-center justify-content-center" style = "background-color:white;" 
+            />
+               
+            <button 
+                id = "aumentar" 
+                class = "col-xl-1 border produto-info-h ms-2 h-75 w-25 d-flex align-items-center justify-content-center" 
+            >
+
+                <i class="fa-solid fa-2x fa-plus"></i>
+                
+            </button>
+
+        </div>
+
+        <div id = "frete" class = "row" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+
+                Envio: R$ 50,00
+
+            </div>
+
+        </div>
+
+        <div id = "metodo_envio" class = "row" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2 w-50" >
+
+                Para Brasil via DHL
+
+            </div>
+
+        </div>
+
+        <div id = "prazo_entrega" class = "row" >
+
+            <div id = "produto-info-h" class = "col-xl-5 ms-2" >
+
+                Estimativa de entrega: 06 Mai
+
+            </div>
+           
+        </div>
+
+        <div id = "" class = "row mt-1 d-flex align-items-center" >
+
+            <div id = "foto_main" class = "col-xl-3 ms-2 d-flex justify-content-center " >
+
+                <button id = "btn-compra" class = "d-flex justify-content-center align-items-center w-100 btn-sz" >
+
+                    Comprar agora
+
+                </button>
+
+            </div>
+
+            <div id = "foto_main" class = "col-xl-3 ms-2 d-flex justify-content-center " >
+
+                <button id="btn-add" class = "d-flex justify-content-center align-items-center w-100 btn-sz" >
+
+                    Adicione ao carrinho
+                    
+                </button>
+
+            </div>
+
+        </div>
+
+    </div> 
+
+    <div id = "" class = "row mt-1 d-flex d-flex justify-content-start" >
+
+        <div id = "fotos-rodape" class = "col-xl-2 d-flex justify-content-center " >
+
+            <div id = "img1" class = "d-flex justify-content-center align-items-center w-25  btn-sz border border-dark text-dark" >
+
+                Img 1
+
+            </div>
+
+            <div id="img2" class = "d-flex justify-content-center align-items-center w-25 btn-sz border border-dark ms-1 text-dark" >
+
+                Img 2
+                
+            </div>
+
+            <div id="img3" class = "d-flex justify-content-center align-items-center w-25 btn-sz border border-dark ms-1 text-dark" >
+
+                Img 3
+                
+            </div>
+
+            <div id = "img4" class = "d-flex justify-content-center align-items-center w-25 btn-sz border border-dark ms-1 text-dark" >
+
+                Img 4
+
+            </div>
+
+            <div id="img5" class = "d-flex justify-content-center align-items-center w-25 btn-sz border border-dark ms-1 text-dark" >
+
+                Img 5
+                
+            </div>
+
+            <div id="img6" class = "d-flex justify-content-center align-items-center w-25 btn-sz border border-dark ms-1 text-dark" >
+
+                Img 6
+                
+            </div>
+
+        </div>
+        
+
+    </div>
+
+
+
+</div>
+
+<!--
+
+<div id = "fotos_categorias" class = "row" >
+
+    <div id = "menu_categorias" class = "col-xl-3 border border-dark" >
+
+        <div id = "categoria1" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria1a" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria2a" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria3a" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "categoria2" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria1b" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria2b" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+        <div id = "subcategoria3b" class = "row" >
+
+            <div id = "" class = "col-xl-2 border border-dark" >
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div id = "menu_categorias" class = "col-xl-8 border border-dark" >
+
+        <div id = "foto1" class = "row" >
+
+            <div id = "" class = "col-xl-5 border border-dark" >
+
+            </div>
+            
+        </div>
+
+        <div id = "foto1" class = "row" >
+
+            <div id = "" class = "col-xl-5 border border-dark" >
+
+            </div>
+            
+        </div>
+
+        <div id = "foto1" class = "row" >
+
+            <div id = "" class = "col-xl-5 border border-dark" >
+
+            </div>
+            
+        </div>
+
+    </div>
+
+</div>-->
+
+<?php include 'footer.php.'; ?>
