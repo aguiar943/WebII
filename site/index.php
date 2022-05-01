bfc7c2f0d4fa9688feed8af71fed20a8aefe6b3b
Rodrigo Stochero <rsaguiar@ucs.br>
2022-05-01 13:16:07 -0300

    ajustes
    adiciona itens a tela principal

--- site/index.php
+++ site/index.php
@@ -11,36 +11,153 @@ include_once "fachada.php";
 -->
 
 	<div class="container ">
-		
+		<br>
 		<main>
-
-			<div class="row g-5 mt-4">
-				
-				<div class="col-md-7 col-lg-12">
-
-					<h4 class="mb-3">Lista</h4>
-
-					<hr>
-
-					<form class="needs-validation" novalidate>
-
-						
-						<div class="row g-3">
-
-							Em construção ... [ 14 / 04 ~ 17 / 04/ ]
-
-							<hr>
-
+			<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/produtos/1.jpg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
 						</div>
-
-						<a href="#" class = " btn btn-primary btn-lg" >Início</a>
-
-						<!-- <a href="editaVeiculo.php" class = " btn btn-primary btn-lg" >Início</a> -->
-
-					</form>
-					
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$4.900,90</h2>
+					</div>
+				  </div>
 				</div>
-
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/produtos/2.jpg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 2</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/produtos/3.jpg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 3</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/produtos/4.jpg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/produtos/5.jpg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>
+				<div class="col">
+				  <div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-body">
+					  <ul class="list-unstyled mt-3 mb-4">
+						<div class="col-md-5">
+							<img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" src="imagens/carrinho.png" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em"></text></img>
+						</div>
+					  </ul>
+					  <h5 class="card-title pricing-card-title">Produto 1</h5>
+					  <h2 class="card-title pricing-card-title">R$49,90</h2>
+					</div>
+				  </div>
+				</div>''
+				
 			</div>
 
 		</main>
