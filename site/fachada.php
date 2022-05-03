62b0c6cfca631ed9ce69b6a94b5411e519474da1
mauricio-esuk <83623933+mauricio-esuk@users.noreply.github.com>
2022-05-03 02:02:29 -0300

    Update fachada.php
    Acrescentado o include para as classes Dao do Produto, Subcategoria, Cor e Marca

--- site/fachada.php
+++ site/fachada.php
@@ -2,7 +2,18 @@
 
 
 include_once('model/Usuario.php');
+include 'model/Cor.php';
+include 'model/Subcategoria.php';
+include 'model/Marca.php';
+include 'model/Produto.php';
+
 include_once('dao/UsuarioDao.php');
+include 'dao/CoresDao.php';
+include 'dao/SubcategoriasDao.php';
+include 'dao/MarcasDao.php';
+include 'dao/ProdutosDao.php';
+include 'dao/Auth.php';
+
 include_once('dao/DaoFactory.php');
 include_once('dao/PostgresDaoFactory.php');
 
