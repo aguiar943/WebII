eb85b81b817da1ddba28fab322d43aa3a1ad3cc9
mauricio-esuk <83623933+mauricio-esuk@users.noreply.github.com>
2022-05-03 02:24:03 -0300

    Update DaoFactory.php

--- site/dao/DaoFactory.php
+++ site/dao/DaoFactory.php
@@ -4,5 +4,12 @@ abstract class DaoFactory {
     protected abstract function getConnection();
 
     public abstract function getUsuarioDao();
+    
+    public abstract function getSubCategoriaDao();
+
+    public abstract function getProdutoDao();
+
+    public abstract function getCorDao();
+    
 }
 ?>
