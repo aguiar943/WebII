4a5ceb85e0e9fda57fb5e6527edd806ea57cdef0
mauricio-esuk <83623933+mauricio-esuk@users.noreply.github.com>
2022-05-07 14:08:54 -0300

    Update cadastro_produtos.php
    1- Abertura do Body já está incluída no header.php, por isto, foi removida deste arquivo de listagem;
    2 - A sessão de footer e a chamada para os scripts do bootstrap, popper e jquery foram removidas, pois elas já estão incluídas no arquivo footer.php;
    3 - Os echos em PHP que imprimiam as tags foram removidos e as tags ficaram em HTML puro diretamente por questão de organização e facilidade de visualização do código.

--- site/cadastro_produtos.php
+++ site/cadastro_produtos.php
@@ -1,74 +1,98 @@
 <?php 
-require "fachada.php"; 
+
+  require "fachada.php";
+
 ?>
-<body class="bg-light">
+
 <div class="container py-3">
+	
   <div class="" role="document">
-		<div class="modal-content rounded-5 shadow">
-			<form class="p-4 p-md-5 border rounded-3 bg-light">
-				<div class="modal-header p-6 pb-4 border-bottom-0">
-					<h2 class="fw-bold mb-0">Cadastro Produtos</h2>
-				</div>
-				<?php
 
-echo "<section>";
-$dao = $factory->getProdutoDao();
-$produtos = $dao->buscaTodos();
-if($produtos) {
- 
-	echo "<table class='table table-hover table-responsive table-bordered'>";
-	echo "<tr>";
-		echo "<th>Barras</th>";
-		echo "<th>Nome</th>";
-		echo "<th>Marca</th>";
-		echo "<th>Modelo</th>";
-		echo "<th>Preço de venda</th>";
-		echo "<th>Opções</th>";
-	echo "</tr>";
+    <div class="modal-content rounded-5 shadow">
+  
+      <form class="p-4 p-md-5 border rounded-3 bg-light">
+    
+        <div class="modal-header p-6 pb-4 border-bottom-0">
+    
+          <h2 class="fw-bold mb-0"> Cadastro Produtos </h2>
+    
+        </div>
+  
+        <section>
+    
+          <?php
 
-	foreach ($produtos as $produto) {
-		
-		extract($produto);
-		
-		echo "<tr>";
-			echo "<td>{$cd_barras}</td>";
-			echo "<td>{$subcategoria}</td>";
-			echo "<td>{$marca}</td>";
-			echo "<td>{$modelo}</td>";
-			echo "<td>{$preco_venda}</td>";
-		
-			echo "<td>";
-				echo "<a href='mostra_usuario.php?cd_barras={$cd_barras}' class='btn btn-primary left-margin'>";
-					echo "<span class='glyphicon glyphicon-list'></span> Visualizar ";
-				echo "</a>";
-				echo "<a href='modifica_usuario.php?cd_barras={$cd_barras}' class='btn btn-info left-margin'>";
-					echo "<span class='glyphicon glyphicon-edit'></span> Alterar";
-				echo "</a>";
-				echo "<a href='remove_produto.php?cd_barras={$cd_barras}' class='btn btn-danger left-margin'";
-					echo "onclick=\"return confirm('Confirma exclusão do produto?')\">";
-					echo "<span class='glyphicon glyphicon-remove'></span> Excluir";
-				echo "</a>";
-			echo "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-}
- 
-echo "<a href='novo_produto.php' class='btn btn btn-success left-margin'>";
-echo "Novo";
-echo "</a>";
+          $dao = $factory->getProdutoDao();
+          $produtos = $dao->buscaTodos();
 
-echo "</section>";
-?>	
-			</form>
-		</div>
-	</div>
+          if($produtos) { ?>
+
+            <table class='table table-hover table-responsive table-bordered'>
+
+              <tr>
+
+                <th> Barras </th>
+                <th> Nome </th>
+                <th> Marca </th>
+                <th> Modelo </th>
+                <th> Preço de venda </th>
+                <th> Opções </th>
+
+              </tr>
+
+                <?php
+
+                foreach ($produtos as $produto) {
+
+                  extract($produto); ?>
+
+                  <tr>
+
+                    <td> <?= $cd_barras; ?> </td>
+                    <td> <?= $subcategoria; ?> </td>
+                    <td> <?= $marca; ?> </td>
+                    <td> <?= $modelo; ?> </td>
+                    <td> <?= $preco_venda; ?> </td>
+
+                    <td> 
+
+                      <a href='mostra_usuario.php?cd_barras=<?= $cd_barras ?>' class='btn btn-primary left-margin'>
+
+                        <span class='glyphicon glyphicon-list'></span> Visualizar
+
+                      </a>
+
+                      <a href='mostra_usuario.php?cd_barras=<?= $cd_barras ?>' class='btn btn-info left-margin ms-2'>
+
+                        <span class='glyphicon glyphicon-edit'></span> Alterar
+
+                      </a>
+
+                      <a href='remove_produto.php?cd_barras=<?= $cd_barras ?>' class='btn btn-danger left-margin ms-2'
+                        onclick = "return confirm('Confirma exclusão do produto?')"   
+                      >
+
+                        <span class='glyphicon glyphicon-remove'></span> Excluir
+
+                      </a>
+
+                    </td>
+
+                  </tr> <?php
+
+                } ?>
+            </table> <?php
+          } ?>
+          <a href='novo_produto.php' class='btn btn btn-success left-margin'> Novo </a>
+    
+        </section>
+
+      </form>
+      
+    </div>
+	   
+  </div>
+
+</div>
+	
 <hr class="featurette-divider">
-  <footer class="container">
-    <p>&copy; 2022–2022 Web II &middot; <a href="index.php">Compre Aí</a> &middot;</p>
-  </footer>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>	
-</html>
