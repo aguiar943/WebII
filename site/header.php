0e7b441f7eec24d7af46e9cb0d8af3190b6ae046
Rodrigo Stochero <rsaguiar@ucs.br>
2022-06-07 20:20:42 -0300

    Update header.php
    ajustes de nomes

--- site/header.php
+++ site/header.php
@@ -12,10 +12,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
-        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-        <meta name="generator" content="Hugo 0.88.1">
         <link rel="icon" href="imagens/simbolo.png" />
-        <title>Checkout example · Bootstrap v5.1</title>
+        <title>Compre Aí</title>
 		
         <link href="css/custom.css" rel="stylesheet" >     
         <link href="css/form-validation.css" rel="stylesheet" >
