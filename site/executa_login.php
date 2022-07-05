2b776c1a5ad1dcd55bfedab056338af6a4e4674a
Rodrigo Stochero <rsaguiar@ucs.br>
2022-07-05 13:19:52 -0300

    Update executa_login.php
    AJUSTADO TELA DE LOGIN

--- site/executa_login.php
+++ site/executa_login.php
@@ -1,17 +1,9 @@
 <?php 
-// Métodos de acesso ao banco de dados 
 require "fachada.php"; 
- 
-// Inicia sessão 
-// session_start();
-
-// Recupera o login 
 $login = isset($_POST["login"]) ? addslashes(trim($_POST["login"])) : FALSE; 
-// Recupera a senha, a criptografando em MD5 
+
 $senha = isset($_POST["senha"]) ? md5(trim($_POST["senha"])) : FALSE; 
-//$senha = isset($_POST["senha"]) ? trim($_POST["senha"]) : FALSE; 
- 
-// Usuário não forneceu a senha ou o login 
+
 if(!$login || !$senha) 
 { 
     echo "Você deve digitar sua senha e login!<br>"; 
@@ -27,15 +19,16 @@ if($usuario) {
     // Agora verifica a senha 
     if(!strcmp($senha, $usuario->getSenha())) 
     { 
-        // TUDO OK! Agora, passa os dados para a sessão e redireciona o usuário 
         $_SESSION["nome_usuario"] = stripslashes($usuario->getNome()); 
         //$_SESSION["permissao"]= $dados["postar"]; 
-        header("Location: index.php"); 
+        echo "<script>alert('Usuário logado com sucesso.');</script>"; 
+        echo "<meta http-equiv='refresh' content='0, url=index.php'>";   
         exit; 
     } else {
         $problemas = TRUE; 
     }
 } else {
+    echo "<script>alert('Usuário e senha não correspondem.'); history.back();</script>";
     $problemas = TRUE; 
 }
 
